32c2d1583fd4e60ed32427d94130fe4bd21ea558
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2024-02-27 00:18:58 -0500

    implement delete option by swiping left but not working yet; present add task modally

--- COMP3097_Group16/ViewController.swift
+++ COMP3097_Group16/ViewController.swift
@@ -2,7 +2,6 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
     
-    // Define the Task struct
     struct Task {
         let name: String
         let description: String
@@ -21,7 +20,6 @@ class ViewController: UIViewController, UITableViewDataSource {
         }
     }
     
-    // Array to store tasks
     var tasks = [Task]()
     
     @IBOutlet weak var tableForUpcoming: UITableView!
@@ -29,15 +27,12 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Example tasks
         let task1 = Task(name: "Project", description: "Complete project proposal", category: "Work", date: "23-02-2024", time: "09:00", status: "complete")
         let task2 = Task(name: "Gym", description: "Go grocery shopping", category: "Personal", date: "23-02-2024", time: "17:30", status: "complete")
         let task3 = Task(name: "Clean Room", description: "Clean Room", category: "Personal", date: "23-02-2024", time: "17:30", status: "complete")
-        
         let task4 = Task(name: "Doctor appointment", description: "Go to the docotr", category: "Personal", date: "23-02-2024", time: "17:30", status: "complete")
         
         
-        // Add tasks to the array
         tasks.append(task1)
         tasks.append(task2)
         tasks.append(task3)
@@ -46,19 +41,29 @@ class ViewController: UIViewController, UITableViewDataSource {
         tableForUpcoming.dataSource = self
         
     }
-
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasks.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let dataTask = tasks[indexPath.row]
         let cell = tableForUpcoming.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
         cell.title.text = dataTask.name
         cell.category.text = "Category: "+dataTask.category
         cell.date.text = "\(dataTask.date)"
-     
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+        }
     }
 }
