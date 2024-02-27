70829a2acf41f77ab24c6cde2152980952bda59f
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2024-02-27 16:22:14 -0500

    initialize coredata

---
+++ COMP3097_Group16/Task/Task+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Task+CoreDataClass.swift
+//  COMP3097_Group16
+//
+//  Created by Jerome Delos Reyes on 2024-02-27.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
---
+++ COMP3097_Group16/Task/Task+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  Task+CoreDataProperties.swift
+//  COMP3097_Group16
+//
+//  Created by Jerome Delos Reyes on 2024-02-27.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Task {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Task> {
+        return NSFetchRequest<Task>(entityName: "Task")
+    }
+
+    @NSManaged public var category: String?
+    @NSManaged public var date: Date?
+    @NSManaged public var details: String?
+    @NSManaged public var name: String?
+    @NSManaged public var status: String?
+    @NSManaged public var time: Date?
+
+}
+
+extension Task : Identifiable {
+
+}
